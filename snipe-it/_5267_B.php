8e117fd6b113b19122e52b474f45924c964c2463
Andrew MacIsaac <macisaac.andrew@gmail.com>
2024-07-11 16:17:48 +0000

    feat: Add a new Snipe-IT label layout
    Use the Avery 5267/8927 layout, and insert QR code with up to three
    plain fields to the right.

---
+++ snipe-it/_5267_B.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Models\Labels\Sheets\Avery;
+
+
+class _5267_B extends _5267
+{
+    private const BARCODE_SIZE   =   0.175;
+    private const BARCODE_MARGIN =   0.020;
+    private const FIELD_SIZE     =   0.150;
+    private const FIELD_MARGIN   =   0.012;
+
+    public function getUnit() { return 'in'; }
+
+    public function getLabelMarginTop()    { return 0.02; }
+    public function getLabelMarginBottom() { return 0.02; }
+    public function getLabelMarginLeft()   { return 0.04; }
+    public function getLabelMarginRight()  { return 0.04; }
+
+    public function getSupportAssetTag()  { return false; }
+    public function getSupport1DBarcode() { return false; }
+    public function getSupport2DBarcode() { return true; }
+    public function getSupportFields()    { return 3; }
+    public function getSupportLogo()      { return false; }
+    public function getSupportTitle()     { return false; }
+
+    public function preparePDF($pdf) {}
+
+    public function write($pdf, $record) {
+        $pa = $this->getLabelPrintableArea();
+
+        $usableWidth = $pa->w;
+        $currentX = $pa->x1;
+        $currentY = $pa->y1;
+
+        $barcodeSize = $pa->h;
+
+        if ($record->has('barcode2d')) {
+            static::write2DBarcode(
+                $pdf, $record->get('barcode2d')->content, $record->get('barcode2d')->type,
+                $pa->x1, $pa->y1,
+                $barcodeSize, $barcodeSize
+            );
+            $currentX += $barcodeSize + self::BARCODE_MARGIN;
+            $usableWidth -= $barcodeSize + self::BARCODE_MARGIN;
+        }
+
+        foreach ($record->get('fields') as $field) {
+            static::writeText(
+                $pdf, $field['value'],
+                $currentX, $currentY,
+                'freemono', 'B', self::FIELD_SIZE, 'L',
+                $usableWidth, self::FIELD_SIZE, true
+            );
+            $currentY += self::FIELD_SIZE + self::FIELD_MARGIN;
+        }
+    }
+}
+
+?>
